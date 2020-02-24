fd9ac4724d75d085b8de69ae980ce1c60834f376
Eugen-Bleck <eugenbleck@gmail.com>
2020-02-24 13:52:59 +0100

    Sets default rate and context for currency

--- affiliates-formula-custom-rate-percentages.php
+++ affiliates-formula-custom-rate-percentages.php
@@ -23,7 +23,8 @@ class Affiliates_Custom_Formula_Rates {
 	}
 
 	/**
-	 * Function Desc
+	 * Sets new variable depending on previous months referrals
+	 *
 	 * @param array $variables
 	 * @param object $rate
 	 * @param array $context
@@ -31,11 +32,16 @@ class Affiliates_Custom_Formula_Rates {
 	 */
 	public static function affiliates_formula_computer_variables( $variables, $rate, $context ) {
 
-		// 1. Get the monthly sales referred by each affiliate
+		// Get the monthly sales referred by each affiliate
 		$affiliate_id = $context['affiliate_id'];
 		$totals       = self::get_affiliate_referrals( $affiliate_id );
-		// 2. Depending on the amount referred for the month
-		// @todo you also need to set the limits aka $first_limit, $second_limit etc.
+		// Get the currency
+		if ( function_exists( 'get_woocommerce_currency' ) ) {
+			$currency = get_woocommerce_currency();
+		} else {
+			$currency = get_option( 'woocommerce_currency' );
+		}
+		// Depending on the amount referred for the month
 		$first_limit   = 28;
 		$second_limit  = 50;
 		$third_limit   = 120;
@@ -43,28 +49,31 @@ class Affiliates_Custom_Formula_Rates {
 		$fifth_limit   = 500;
 		$sixth_limit   = 1000;
 		$seventh_limit = 2500;
-		// @todo before the ending else statement you need to add the rest of cases applying for each amount.		
-		if ( isset( $totals['amount'] ) ) {
-			if ( $totals['amount'] >= 0 && $totals['amount'] <= $first_limit ) {
-				$variables['c'] = 0.035;
-			} elseif ( $totals['amount'] >= $first_limit && $totals['amount'] <= $second_limit ) {
-				$variables['c'] = 0.07;
-			} elseif ( $totals['amount'] >= $second_limit && $totals['amount'] <= $third_limit ) {
-				$variables['c'] = 0.105;
-			} elseif ( $totals['amount'] >= $third_limit && $totals['amount'] <= $forth_limit ) {
-				$variables['c'] = 0.14;
-			} elseif ( $totals['amount'] >= $forth_limit && $totals['amount'] <= $fifth_limit ) {
-				$variables['c'] = 0.175;
-			} elseif ( $totals['amount'] >= $fifth_limit && $totals['amount'] <= $sixth_limit ) {
-				$variables['c'] = 0.21;
-			} else {
-				$variables['c'] = 0.25;
+
+		// The default value for the variable
+		$variables['c'] = 0.035;
+
+		// Check the previous month's performance and adjust the variable value
+		foreach ( $totals as $total ) {
+			if ( isset( $totals[$currency] ) ) {
+				if ( $total[$currency] >= 0 && $total[$currency] <= $first_limit ) {
+					$variables['c'] = 0.035;
+				} else if ( $total[$currency] >= $first_limit && $total[$currency] <= $second_limit ) {
+					$variables['c'] = 0.07;
+				} else if ( $total[$currency] >= $second_limit && $total[$currency] <= $third_limit ) {
+					$variables['c'] = 0.105;
+				} else if ( $total[$currency] >= $third_limit && $total[$currency] <= $forth_limit ) {
+					$variables['c'] = 0.14;
+				} else if ( $total[$currency] >= $forth_limit && $total[$currency] <= $fifth_limit ) {
+					$variables['c'] = 0.175;
+				} else if ( $total[$currency] >= $fifth_limit && $total[$currency] <= $sixth_limit ) {
+					$variables['c'] = 0.21;
+				} else {
+					$variables['c'] = 0.25;
+				}
 			}
 		}
-		// 2.1 Apply on the the following percentages 3.50% 7% 10.50% 14% 17.50% 21% 25%
-		// 3. Credit the affiliate with the right new computed amount as his monthly referral
 
-		//return $rates;
 		return $variables;
 	}
 
@@ -81,19 +90,19 @@ class Affiliates_Custom_Formula_Rates {
 		$results         = $wpdb->get_results( $wpdb->prepare(
 			"
 			SELECT SUM(amount) as total, currency_id
-			FROM $referrals_table 
+			FROM $referrals_table
 			WHERE YEAR(datetime) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
 			AND MONTH(datetime) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-			AND affiliate_id = $affiliate_id
+			AND affiliate_id = %d
 			AND status = 'accepted'
 			GROUP BY currency_id
-			"
+			",
+			$affiliate_id
 		) );
 		if ( count( $results ) > 0 ) {
 			foreach ( $results as $result ) {
 				$totals[] = array(
-					'amount'   => esc_html( affiliates_format_referral_amount( $result->total, 'display' ) ),
-					'currency' => esc_html( $result->currency_id )
+					$result->currency_id => esc_html( affiliates_format_referral_amount( $result->total, 'display' ) )
 				);
 			}
 		}
